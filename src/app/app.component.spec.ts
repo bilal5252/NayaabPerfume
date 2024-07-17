8f51e1fc45262eafa52670c3aa8b6bdb8e13269b
Bilal Momin <56248858+bilal5252@users.noreply.github.com>
2024-07-17 11:23:59 +0530

    name changes added

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'nayaab-perfume' title`, () => {
+  it(`should have the 'NayaabPerfume' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('nayaab-perfume');
+    expect(app.title).toEqual('NayaabPerfume');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, nayaab-perfume');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, NayaabPerfume');
   });
 });
